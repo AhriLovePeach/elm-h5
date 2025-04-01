29e4a901b5c93e947f456104cb33fcd5668f7376
zhoufei0226 <157690910@qq.com>
2025-04-01 14:51:30 +0800

    head

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,54 +1,21 @@
 <script setup lang="ts">
-import { ref, toRefs, reactive, defineProps } from 'vue'
+import { defineProps } from 'vue'
 
 defineProps<{
   msg: string
 
 }>()
-// console.log('setup')
-// onBeforeMount(() => {
-//   console.log('onBeforeMount');
-// })
-// onMounted(() => {
-//   console.log('onMounted');
-// })
-// onBeforeUpdate(() => {
-//   console.log('onBeforeUpdate');
-// })
-// onUpdated(() => {
-//   console.log('onUpdated');
-// })
-// onBeforeUnmount(() => {
-//   console.log('onBeforeUnmount');
-// })
-// onUnmounted(() => {
-//   console.log('onUnmounted');
-// })
-const msgRef = ref('aaaa')
-const msg2 = reactive({
-  age: 22,
-  name: '大虎'
-})
-const { age, name } = toRefs(msg2)
 
-function checkBtn() {
-  msgRef.value = msgRef.value + Date.now()
-  msg2.age++
-}
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h1 class="green">{{ msgRef }}</h1>
-    <h1 class="green">{{ age }}</h1>
-    <h1 class="green">{{ name }}</h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
-    <button type="button" @click="checkBtn">change msg</button>
   </div>
 </template>
 
