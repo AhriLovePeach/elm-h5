70d75717764e5a0e695c99fd0fcf3e34bbf3f9d7
zhoufei0226 <157690910@qq.com>
2025-04-03 18:38:55 +0800

    feat: 添加了json-server服务mock数据

---
+++ src/views/tabs/TabsView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { RouterView, useRoute, useRouter } from 'vue-router'
+import { Tabbar, TabbarItem } from 'vant';
+import { ref, watch } from "vue";
+const route = useRoute()
+const router = useRouter()
+const activeRef = ref(route.name as string)
+watch(activeRef, (newName) => {
+    router.push({
+        name: newName
+    })
+});
+</script>
+<template>
+    <RouterView />
+    <Tabbar v-model="activeRef">
+        <TabbarItem name="home" icon="home-o">首页</TabbarItem>
+        <TabbarItem name="order" icon="bars">订单</TabbarItem>
+        <TabbarItem name="me" icon="contact">我的</TabbarItem>
+    </Tabbar>
+</template>
+<style>
+.tit {
+    text-align: center;
+}
+</style>
