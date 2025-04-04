fae434baf4be0f7c3c766ffb71e12fbdc15256ab
zhoufei0226 <157690910@qq.com>
2025-04-04 15:16:00 +0800

    feat: 添加postcss插件

--- .history/src/main_20250403152554.ts
+++ .history/src/main_20250403152554.ts
@@ -10,9 +10,4 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-const rootValue = 16
-const rootWidth = 390
-const deviceWidth = document.documentElement.clientWidth
-document.documentElement.style.fontSize = `${(deviceWidth / rootWidth) * rootValue}px`
-
 app.mount('#app')
